cd6a9b808b144ac9162a8e1e0e3bb1ea2007b6d9
no135 <nobodyst1234@gmail.com>
2025-03-27 23:32:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var options = {
+    strings: ["Dreamers, Thinkers, and Doers"], 
+    typeSpeed: 50,  
+    backSpeed: 0,  
+    backDelay: 1000,  
+    showCursor: true,  
+    loop: true,  
+    fadeOut: true, 
+    fadeOutClass: "typed-fade-out", 
+    fadeOutDelay: 500 
+};
+var typed = new Typed("#typed", options); 
